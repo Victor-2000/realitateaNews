66ead904054b950d0b5551d9e5e7c52c254eefd8
Victor Ciobanu <1908965@swansea.ac.uk>
2021-01-07 21:19:13 +0200

    Finished heading nav with the weather and currency buttons

--- js/script.js
+++ js/script.js
@@ -1,14 +1,53 @@
 
 function setupAds(){
-    var adblock = document.querySelectorAll(".adblock");
+    let adblock = document.querySelectorAll(".adblock");
     
     adblock.forEach(setCancelButton);
 }
 
 function setCancelButton(item){
-    var closeIcon = item.querySelectorAll(".close-icon");
-    closeIcon[0].addEventListener("click", function(){
-        item.style.display = "none";});
+    let closeIcon = item.querySelectorAll(".close-icon");
+    closeIcon[0].addEventListener("click", 
+    function(){
+        item.style.display = "none";
+    });
+}
+
+function setupAdditionalInfo(){
+    let currencyButton = document.querySelector(".currency-button");
+    let weatherButton = document.querySelector(".weather-button");
+    let infoSection = document.querySelector(".additional-info");
+    let currencyBlock = document.querySelector(".curs-valutar");
+    let weatherBlock = document.querySelector(".meteo");
+    let background = document.querySelector(".additional-info-background");
+
+    let isCurrency = false;
+
+    currencyButton.addEventListener("click", 
+    function(){
+        infoSection.style.display = "flex";
+        currencyBlock.style.display = "block";
+        isCurrency = true;
+    });
+
+    weatherButton.addEventListener("click", 
+    function(){
+        infoSection.style.display = "flex";
+        weatherBlock.style.display = "block";
+        isCurrency = false;
+    });
+
+
+    background.addEventListener("click", 
+    function(){
+        infoSection.style.display = "none";
+        if(isCurrency){
+            currencyBlock.style.display = "none";
+        }else{
+            weatherBlock.style.display = "none";
+        }
+    });
 }
 
 setupAds();
+setupAdditionalInfo();
