ff4e664ded743da0817aa218e6964607d94bacf3
Victor Ciobanu <1908965@swansea.ac.uk>
2021-01-18 12:23:43 +0200

    Sorted out the javascript for the menus

--- js/script.js
+++ js/script.js
@@ -75,6 +75,89 @@ function positionTitle(item){
     }
 }
 
+function fadeIn(button, target, closeButton = null){
+    button.addEventListener("click",
+        function () {
+            target.style["display"] = "block";
+            let op = 0;
+            for(let i=0;i<10;i++){
+                setTimeout(
+                    function () {
+                        op+=0.1;
+                        target.style["opacity"] = op;
+                        if(op > 0.9){
+                            if(closeButton === null){
+                                fadeOut(button,target);
+                            }else{
+                                fadeOut(closeButton,target,button);
+                            }
+                        }
+                    }
+                    ,30);
+            }
+        }
+    );
+}
+
+function fadeOut(button, target, openButton = null){
+    button.addEventListener("click",
+        function () {
+            let op = 1;
+            setTimeout(function () {
+                target.style["display"] = "none";
+            },300);
+            for(let i=0;i<10;i++){
+                setTimeout(
+                    function () {
+                        op-=0.1;
+                        target.style["opacity"] = 0;
+                        if(op < 0.1){
+                            if(openButton === null){
+                                fadeIn(button,target);
+                            }   
+                        }
+                    }
+                    ,30);
+            }
+        }
+    );
+}
+
+function slideIn(button, target) {
+    button.onclick = function () {
+        setTimeout(
+            function () {
+                target.style["bottom"] = 0;
+                slideOut(button,target);
+            }
+        ,1);
+    }
+}
+
+function slideOut(button, target) {
+    button.onclick = function () {
+        setTimeout(
+            function () {
+                target.style["bottom"] = "100px";
+                slideIn(button,target);
+            }
+        ,1);
+    }
+}
+
+function setupMobileHeader(){
+    let menuButton = document.querySelector(".menu-icon");
+    let menuCloseButton = document.querySelector(".close-menu-icon");
+    let searchButton = document.querySelector(".search-icon-header");
+
+    let menuTarget = document.querySelector(".menu");
+    let searchTarget = document.querySelector(".search-box");
+
+    fadeIn(menuButton,menuTarget,menuCloseButton);
+    slideIn(searchButton, searchTarget);
+}
+
+setupMobileHeader();
 setupSmallArticles();
 setupBody();
 setupAds();
